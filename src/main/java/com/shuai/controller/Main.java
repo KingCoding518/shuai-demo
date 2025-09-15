0274d2c49b88648c59defe1c0aad38dfc80ddb67
王帅 <2895788257@qq.com>
2025-09-15 12:34:30 +0800

    feat: 测试git合并代码

--- src/main/java/com/shuai/controller/Main.java
+++ src/main/java/com/shuai/controller/Main.java
@@ -22,6 +22,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Runtime.getRuntime().availableProcessors());
-        System.out.println("test");
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
     }
 }
